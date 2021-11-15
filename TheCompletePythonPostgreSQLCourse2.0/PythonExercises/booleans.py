63eda23c6d035fa81ccc9901e60f2576a9c4fae9
rominalodolo <lodoloromina@gmail.com>
2021-11-15 18:21:08 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/booleans.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/booleans.py
@@ -6,3 +6,7 @@ print(10 != 10)
 
 # comparrisions: ==, !=, >, <, >=, <= 
 
+friends = ["Romi", "Amy", "Chels", "Mary", "Ally", "John"]
+abroad = ["Romi", "Amy", "Chels", "Mary"]
+
+print(friends == abroad)
