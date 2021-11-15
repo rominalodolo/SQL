8db7cb4f986782306572871c480e9350520d987c
rominalodolo <lodoloromina@gmail.com>
2021-11-15 18:24:11 +0200

    updates

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/advanced_set_operations.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/advanced_set_operations.py
@@ -32,3 +32,40 @@ science = {"Romina", "Mellisa", "Rory", "Sally", "Adam"}
 
 both = art.intersection(science)
 print(both)
+
+
+# MORE on Set Operators 
+
+set_1 = {1, 2, 3, 4, 5}
+set_2 = {3, 4, 5, 6, 7}
+
+# Combine set_1 and set_2
+print(set_1.union(set_2))  # {1, 2, 3, 4, 5, 6, 7}
+
+# Find common elements in set_1 and set_2
+print(set_1.intersection(set_2))  # {3, 4, 5}
+
+# Find elements in set_1 which are not in set_2
+print(set_1.difference(set_2))  # {1, 2}
+
+set_1 = {1, 2, 3, 4, 5}
+set_2 = {3, 4, 5, 6, 7}
+
+# Combine set_1 and set_2
+print(set_1 | set_2)  # {1, 2, 3, 4, 5, 6, 7}
+
+# Find common elements in set_1 and set_2
+print(set_1 & set_2)  # {3, 4, 5}
+
+# Find elements in set_1 which are not in set_2
+print(set_1 - set_2)  # {1, 2}
+
+
+set_1 = {1, 2, 3, 4, 5}
+set_2 = {3, 4, 5, 6, 7}
+set_3 = {5, 6, 7, 8, 9}
+
+# Combine set_1 and set_2 and set_3
+print(set_1 | set_2 | set_3)  # {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    # https://blog.teclado.com/python-set-operators/ 
