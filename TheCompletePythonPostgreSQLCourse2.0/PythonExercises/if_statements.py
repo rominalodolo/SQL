cc374e78371bf3b491ef77e5c4fe267523c57d7a
rominalodolo <lodoloromina@gmail.com>
2021-11-15 18:52:06 +0200

    if statements

---
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/if_statements.py
@@ -0,0 +1,14 @@
+# if statements 
+
+day_of_week = input("What day of the week is it today?")
+
+print(day_of_week == "Monday")
+
+
+# putting in the if statement 
+
+
+day_of_week = input("What day of the week is it today?")
+
+if day_of_week == "Monday":
+    
