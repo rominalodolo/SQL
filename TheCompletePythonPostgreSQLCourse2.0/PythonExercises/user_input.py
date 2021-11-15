3ee0bab2b137e3531f7724f652d8cdbffc817690
rominalodolo <lodoloromina@gmail.com>
2021-11-15 17:35:49 +0200

    user input

---
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/user_input.py
@@ -0,0 +1,11 @@
+# User input 
+
+name = input("Enter your name: ")
+print(name)
+
+# encoding maths (you have to convert the string to number)
+
+size_input = input("How big is your house (in square feet): ")
+square_feet = int(size_input)
+square_meters = size_input / 10.8 
+print(square_meters)
