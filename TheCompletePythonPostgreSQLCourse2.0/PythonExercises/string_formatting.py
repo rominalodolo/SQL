e5bc0a6cd97b074d6753e8350fb1c4adf6fc0c22
rominalodolo <lodoloromina@gmail.com>
2021-11-15 17:30:28 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/string_formatting.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/string_formatting.py
@@ -11,3 +11,11 @@ name = "Romina"
 greeting = "Hello, {}"
 with_name = greeting.format(name)
 
+print(with_name)
+
+# Longer template 
+
+longer_phrase = "Hello, {}. Today is {}."
+
+formatted = longer_phrase.format("Romina", "Monday")
+print(formatted)
