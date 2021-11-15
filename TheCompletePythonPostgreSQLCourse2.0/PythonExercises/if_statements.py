7492b3331860a0f21a6a36741a59ef0ef5b48802
rominalodolo <lodoloromina@gmail.com>
2021-11-15 19:00:56 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/if_statements.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/if_statements.py
@@ -8,9 +8,15 @@ print(day_of_week == "Monday")
 # putting in the if statement 
 
 
-day_of_week = input("What day of the week is it today?")
+day_of_week = input("What day of the week is it today?").lower()
 
-if day_of_week == "Monday":
+if day_of_week == "monday":
     print("Have a great start to your week!")
+elif day_of_week == "tuesday":
+    print("At least it's not Monday.")
+else: 
+    print("Good luck!")
 
 print("It's going to be a long week..")
+
+#adding lower() so that you can have varied case in your answers 
