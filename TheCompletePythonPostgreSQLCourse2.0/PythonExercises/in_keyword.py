cdd20bdd9cff53b3dabdec73367ac7abe6d9abec
rominalodolo <lodoloromina@gmail.com>
2021-11-15 22:44:55 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/in_keyword.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/in_keyword.py
@@ -50,3 +50,17 @@ if user_input in ("y", "Y"):
     else:
         print("Sorry, Guess again.")
 
+
+# different way to code 
+
+number = 6
+user_input = input("Enter 'y' if you would like to play.")
+
+if user_input in ("y", "Y"):
+    user_number = int(input("Guess our number: "))
+    if user_number == number: 
+        print("You guessed correctly!")
+    elif abs(number - user_number) == 1:
+        print("You were off by one.")
+    else:
+        print("Sorry, Guess again.")
