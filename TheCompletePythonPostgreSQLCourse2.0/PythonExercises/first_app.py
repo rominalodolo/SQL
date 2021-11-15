642ec1b150f7247c2913fcaf2587a3b42763e513
rominalodolo <lodoloromina@gmail.com>
2021-11-15 17:44:43 +0200

    update

---
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/first_app.py
@@ -0,0 +1,10 @@
+user_age = input("Enter your age: ")
+years = int(user_age)
+months = years * 12
+print(f"Your age, {years}, is equal to {months} months.")
+
+# more simplified 
+
+user_age = int(input("Enter your age: "))
+months = user_age * 12 
+print(f"Your age, {years}, is equal to {months} months.")
--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/user_input.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/user_input.py
@@ -16,3 +16,11 @@ size_input = input("How big is your house (in square feet): ")
 square_feet = int(size_input)
 square_meters = size_input / 10.8 
 print(f"{square_feet} square feet is {square_meters} square meters.")
+
+
+# formatting a floating number 
+
+size_input = input("How big is your house (in square feet): ")
+square_feet = int(size_input)
+square_meters = size_input / 10.8 
+print(f"{square_feet} square feet is {square_meters:.2f} square meters.")
