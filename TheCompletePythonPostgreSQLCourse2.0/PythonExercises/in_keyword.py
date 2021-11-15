530f64fa877c32afeeeb34bf8e35389210fa1d2c
rominalodolo <lodoloromina@gmail.com>
2021-11-15 22:39:26 +0200

    keyword

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/in_keyword.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/in_keyword.py
@@ -45,6 +45,8 @@ if user_input in ("y", "Y"):
     user_number = int(input("Guess our number: "))
     if user_number == number: 
         print("You guessed correctly!")
+    elif number - user_number in (1, -1):
+        print("You were off by one.")
     else:
         print("Sorry, Guess again.")
 
