b29c01c95022edc3630dc3bde98a8fe018b5f4cb
rominalodolo <lodoloromina@gmail.com>
2021-11-15 17:38:03 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/user_input.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/user_input.py
@@ -9,3 +9,10 @@ size_input = input("How big is your house (in square feet): ")
 square_feet = int(size_input)
 square_meters = size_input / 10.8 
 print(square_meters)
+
+# adding an f string 
+
+size_input = input("How big is your house (in square feet): ")
+square_feet = int(size_input)
+square_meters = size_input / 10.8 
+print(f"{square_feet} square feet is {square_meters} square meters.")
