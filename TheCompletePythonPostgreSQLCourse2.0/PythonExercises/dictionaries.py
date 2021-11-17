334b2715db02998fdea7ef8bfae3d17b61e3944a
rominalodolo <lodoloromina@gmail.com>
2021-11-17 08:53:20 +0200

    loop

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/dictionaries.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/dictionaries.py
@@ -7,4 +7,20 @@ friend_ages["Romi"] = 27
 
 print(friend_ages)
 
-# 
+# Indexing a dictionary 
+
+friends = [
+    {"name": "Romi", "age": 27}
+    {"name": "Amy", "age": 30}
+    {"name": "Jen", "age": 29}
+]
+
+print(friends[1]["name"])
+
+
+# for loops 
+
+student_attendance = {"Romi": 26, "Amy": 30, "Jen": 29}
+
+for student, attendance in student_attendance.items()
+print(f"{student}: {attendance}")
