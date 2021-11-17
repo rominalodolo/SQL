325e97adf7d42b8e4da14d70c2912e83544070ff
rominalodolo <lodoloromina@gmail.com>
2021-11-17 08:41:23 +0200

    lists

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/ListComprehensions.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/ListComprehensions.py
@@ -7,3 +7,15 @@ for num in numbers:
     doubled.append(num * 2)
 
 
+# simplified 
+
+numbers = [1, 3, 5]
+doubled = [x * 2 for x in numbers]
+
+# working with strings
+
+friends = ["Sam", "Johan", "Romi", "Jeff"]
+starts_s = [friend for friend in friends if friend.startswith("S")]
+
+print(starts_s)
+
