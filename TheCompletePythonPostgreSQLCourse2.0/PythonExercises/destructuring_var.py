9b385bb4f4b14b63752df31b521268c8fcc6cde7
rominalodolo <lodoloromina@gmail.com>
2021-11-17 09:01:09 +0200

    t

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/destructuring_var.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/destructuring_var.py
@@ -1,2 +1,14 @@
 # Destructuring variables 
 
+t = 5, 11
+x, y = t
+
+print(x, y)
+
+
+# 
+
+student_attendance = {"Romi": 26, "Amy": 30, "Jen": 29}
+
+for student, attendance in student_attendance.items()
+print(f"{student}: {attendance}")
