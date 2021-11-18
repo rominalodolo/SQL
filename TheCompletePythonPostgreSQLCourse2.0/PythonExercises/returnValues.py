1fdbb683724e0e139f0cfb9db9bdfdafc59077e8
rominalodolo <lodoloromina@gmail.com>
2021-11-18 15:55:33 +0200

    updates

---
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/returnValues.py
@@ -0,0 +1,20 @@
+# Function return values
+
+def add(x, y):
+    print(x + y)
+
+add(5, 8)
+result = add(5, 8)
+print(result)
+
+
+# adding a return function
+
+def add(x, y):
+    print(x + y)
+    return x + y 
+
+result = add(5, 8)
+print(result)
+# answer is 13 twice 
+
