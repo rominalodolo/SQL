ac5d40db5c03bcd43220818be6723ef48d898d89
rominalodolo <lodoloromina@gmail.com>
2021-11-18 15:30:10 +0200

    default NOT do

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/defaultParameterValues.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/defaultParameterValues.py
@@ -4,3 +4,16 @@ def add(x, y=8):
     print(x + y)
 
 add(5)
+
+# Do NOT do this - change after defining the function 
+
+default_y = 3
+
+def add(x, y=default_y):
+    sum = x + y 
+    print(sum)
+
+add(2)
+
+default_y =  4 
+add(2)
