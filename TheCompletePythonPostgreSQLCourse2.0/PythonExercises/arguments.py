465b3936db288ff94b7df6144f2d09a00cbd8d0a
rominalodolo <lodoloromina@gmail.com>
2021-11-18 15:17:20 +0200

    arguments python

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/arguments.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/arguments.py
@@ -2,3 +2,26 @@
 
 def add(x,y):
     pass
+
+add(5, 3)
+
+# OR
+
+def add(x,y):
+    result = x + y 
+    print(result)
+
+add(5, 3)
+
+# No parameters 
+
+def say_hello():
+    print("Hello!")
+
+say_hello("Bob")
+
+# this will result in an ERROR
+
+# Name parameter 
+
+def 
