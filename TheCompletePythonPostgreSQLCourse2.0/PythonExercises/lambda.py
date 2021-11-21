2daa3ed42e9d6df8eaac9635400201e7c7f7d1f5
rominalodolo <lodoloromina@gmail.com>
2021-11-21 16:40:35 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/lambda.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/lambda.py
@@ -15,12 +15,17 @@ lambda x, y: x + y
 print(5, 7)
 
 
- # example 
+# example 
 def double(x):
     return x * 2
 
 sequence = [1, 3, 5 , 9]
-# doubled = [double(x) for x in sequence]
+doubled = [double(x) for x in sequence]
 doubled = map(double, sequence)
 
+# re-writing as a lambda function 
+
+sequence = [1, 3, 5 , 9]
+doubled = [(lambda x: x *2)(x) for x in sequence]
+doubled = list(map(lambda x: x * 2, sequence))
 
