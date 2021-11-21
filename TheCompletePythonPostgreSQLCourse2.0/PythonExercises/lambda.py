ce9939c9839ef19e2d54bc8ee8e57e2566ac3e27
rominalodolo <lodoloromina@gmail.com>
2021-11-21 16:37:30 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/lambda.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/lambda.py
@@ -1,13 +1,26 @@
 # Lambda Function inputs and outputs 
 
+from typing import Sequence
+
+
 def add(x, y):
     return x + y 
 
 print(add(5, 7))
 
-# re-writing as a lambda function 
+# re-writing as a lambda function (short functions)
 
 lambda x, y: x + y 
 
+print(5, 7)
+
+
+ # example 
+def double(x):
+    return x * 2
+
+sequence = [1, 3, 5 , 9]
+# doubled = [double(x) for x in sequence]
+doubled = map(double, sequence)
 
 
