00c6001cd1123dddea6e7ed13cbebcaa948ebebd
rominalodolo <lodoloromina@gmail.com>
2021-11-27 19:33:52 +0200

    dictionary comprehension

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/dictionaryComprehension.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/dictionaryComprehension.py
@@ -1,3 +1,14 @@
 # Dictionary Comprehensions 
 
-users = 
+users = [
+    (0, "Jane", "password"),
+    (1, "Dan", "7091995"),
+    (2, "Jason", "longestPassword12"),
+    (3, "Bobby", "1234"),
+]
+
+username_mapping = {user[1]: user for user in users}
+
+print(username_mapping)
+
+# for user in users: 
