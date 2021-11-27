32302e86535ccbe5671b158e63f0ec964910315a
rominalodolo <lodoloromina@gmail.com>
2021-11-27 19:40:28 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/dictionaryComprehension.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/dictionaryComprehension.py
@@ -11,4 +11,18 @@ username_mapping = {user[1]: user for user in users}
 
 print(username_mapping)
 
-# for user in users: 
+
+    #  alternative way to code the outputs
+
+# for user in users:
+#     if user[1] == "Dan":
+#         print(user)
+
+# user_input = input("Enter your username: ")
+# user_input = input("Enter your password: ")
+# _, username, password =  username_mapping[username_input]
+
+# if password_input == password:
+#     print("Your details are correct!")
+# else: 
+#     print("Your details are incorrect.")
