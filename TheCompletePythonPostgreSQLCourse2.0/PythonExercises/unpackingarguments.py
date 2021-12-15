9db078ad9f1abcb10de0770aa36b5b710dac568b
rominalodolo <lodoloromina@gmail.com>
2021-12-15 15:57:07 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/unpackingarguments.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/unpackingarguments.py
@@ -8,3 +8,13 @@ def multiply(*args):
     
 # multiply(1, 3, 5)
 # print(multiply(-1))
+
+
+#def multiply(*args):
+    print(args)
+    total = 1
+    for arg in args:
+        total * arg
+    
+    return total
+# def apply(*args, operator):
