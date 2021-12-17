51d31e05663a3d0e2a9fc10e524aee7e24defaac
rominalodolo <lodoloromina@gmail.com>
2021-12-17 15:20:40 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/unpackingKeywordArgs.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/unpackingKeywordArgs.py
@@ -6,9 +6,19 @@ named(name="Romi", age=27)
 
 # you can also unpack 
 
-def named(name, age):
-    print(name, age)
+def named(**kwargs:
+    print(kwargs)
 
 details = {"name": "Romi", "age": 27}
 
 named(**details) 
+
+# def named(**kwargs):
+    # print(kwargs)
+       
+ # def print_nicely(**kwargs):
+    # names(**kwargs)
+    # for arg, value in kwargs.items(): 
+    # print(f"{arg}: {value}")
+     
+# print_nicely(name="Romi", age=27)
