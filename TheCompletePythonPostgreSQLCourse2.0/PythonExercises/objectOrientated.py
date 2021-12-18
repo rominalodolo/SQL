ce1cd7e0f0b3defd1484dc366b89a6d380c55946
rominalodolo <lodoloromina@gmail.com>
2021-12-18 16:27:25 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/objectOrientated.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/objectOrientated.py
@@ -1,2 +1,6 @@
 # Object Orientation in Programming 
+"""allows dev to write code in a diff way for simpler code """
 
+student = {"name": "Romi", "grades": (89, 90, 93, 78, 81)}
+
+def average(sequen)
