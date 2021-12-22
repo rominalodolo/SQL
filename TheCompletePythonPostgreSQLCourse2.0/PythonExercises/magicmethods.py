620d17231b5963f1c475a235698095badf2ab61e
rominalodolo <lodoloromina@gmail.com>
2021-12-22 11:50:43 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/magicmethods.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/magicmethods.py
@@ -5,11 +5,11 @@ class Preson:
         self.name = name
         self.age = age
 
-    def __str__(self):
-        return f"Person {self.name}, {self.age} years old."
+    # def __str__(self):
+    #     return f"Person {self.name}, {self.age} years old."
 
-    def __str__(self):
-        return f"Person {self.name}, {self.age} years old."
+    def __repr__(self):
+        return f"<Person ({self.name}, {self.age} years old.)>"
 
 
 bob = Person("Bob", 35)
