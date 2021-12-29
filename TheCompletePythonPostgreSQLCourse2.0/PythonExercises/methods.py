4b273eb2948b1b362309c3b62fb6a9b655e0c3af
rominalodolo <lodoloromina@gmail.com>
2021-12-29 10:45:59 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/methods.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/methods.py
@@ -29,5 +29,20 @@ class Book:
         self.book_type = book_type
         self.weight = weight 
 
-    def __repr__(self) -> str:
-        return
+    def __repr__(self):
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
+
+    @classmethod
+    def hardcover(cls, name, page_weight):
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    @classmethod
+    def paperback(cls, name, page_weight):
+        return Book(name, Book.TYPES[1], page_weight)
+
+book = Book.hardcover("Harry Potter", 1500)
+light = Book.paperback("Python 101", 600)
+
+print(book)
+
+
