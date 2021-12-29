4e7d71095e61403211e4c185b28ea00c0c5fe3e7
rominalodolo <lodoloromina@gmail.com>
2021-12-29 10:41:12 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/methods.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/methods.py
@@ -25,4 +25,9 @@ class Book:
     TYPES = ("hardcover", "paperback")
 
     def __init__(self, name, book_type, wright):
-        self.name = 
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight 
+
+    def __repr__(self) -> str:
+        return
