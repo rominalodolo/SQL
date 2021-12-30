79805a233b5498548be8a0cefe90a5037b6ad221
rominalodolo <lodoloromina@gmail.com>
2021-12-30 21:59:32 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/PythonExercises/classinheritence.py
+++ TheCompletePythonPostgreSQLCourse2.0/PythonExercises/classinheritence.py
@@ -1,2 +1,10 @@
 # Class inheritance 
 
+class Device:
+    def __init__(self, name, connected_by):
+        self.name = name 
+        self.connected_by = connected_by
+        self.connected = TRUE 
+
+    def __str__(self):
+        return f"Device {self.name}"
