a2a132545b3379a97ce4e96ec8c96492564e545c
rominalodolo <lodoloromina@gmail.com>
2022-01-26 12:32:32 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/progdiary/database.py
+++ TheCompletePythonPostgreSQLCourse2.0/progdiary/database.py
@@ -3,11 +3,18 @@ import sqlite3
 
 import sqlite3
 
+from matplotlib.pyplot import connect
+
 
 connection = sqlite3.connect("data.db")
+
+
+def create_table():
+    connection.execute("CREATE TABLE entries (content TEXT, date TEXT);")
+    connection.commit()
 
 def add_entry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date})
 
 def view_entries():
-  return entries
+  return entries 
