8ca4aedac273d791b5a1e9fb13bf3d1b85e88d4a
rominalodolo <lodoloromina@gmail.com>
2022-01-26 10:07:53 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/progdiary/app.py
+++ TheCompletePythonPostgreSQLCourse2.0/progdiary/app.py
@@ -1,4 +1,4 @@
-from database import add_entry, view_entries
+from database import add_entry, get_entries
 
 menu = """Please select one of the following options: 
 1) Add new entry for today.
@@ -8,14 +8,26 @@ Your selection: """
 
 welcome = "Welcome to the programming diary!"
 
+def prompt_new_entry():
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date: ")
+
+    add_entry(add_entry, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+
+
 print(welcome)
 
 user_input = input (menu)
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        add_entry()
+        prompt_new_entry()
     elif user_input == "2":
-        view_entries()
+        entries = get_entries()
+        view_entries(entries)
     else:
         print("Invalid option, please try again.") 
 
