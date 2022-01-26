cb2fba22133577527517adad40cfb08ab4ae30e7
rominalodolo <lodoloromina@gmail.com>
2022-01-26 09:03:02 +0200

    new

---
+++ TheCompletePythonPostgreSQLCourse2.0/progdiary/app.py
@@ -0,0 +1,13 @@
+menu = """Please select one of the following options: 
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+Your selection: """
+
+welcome = "Welcome to the programming diary!"
+
+user_input = input (menu)
+while (user_input := input(menu)) != "3":
+    # We'll deal with user input here...
+
+    user_inout = input(menu)
