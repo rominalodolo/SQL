2b66da29d24b15ed536cba7c27e5b30b3e5a0cff
rominalodolo <lodoloromina@gmail.com>
2022-01-26 11:22:11 +0200

    import

--- TheCompletePythonPostgreSQLCourse2.0/progdiary/database.py
+++ TheCompletePythonPostgreSQLCourse2.0/progdiary/database.py
@@ -1,8 +1,10 @@
 import sqlite3
 
 
-entries = []
-connection = sqlite3
+import sqlite3
+
+
+connection = sqlite3.connect("data.db")
 
 def add_entry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date})
