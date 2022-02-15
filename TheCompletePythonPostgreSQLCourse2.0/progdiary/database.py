baf8084ac27b255f992742dbb055793b047bf844
rominalodolo <lodoloromina@gmail.com>
2022-02-15 11:47:44 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/progdiary/database.py
+++ TheCompletePythonPostgreSQLCourse2.0/progdiary/database.py
@@ -5,7 +5,8 @@ connection = sqlite3.connect("data.db")
 
 def create_table():
     with connection:
-        connection.execute("CREATE TABLE entries (content TEXT, date TEXT);")
+        connection.execute("CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);"
+        )
 
 
 def add_entry(entry_content, entry_date):
