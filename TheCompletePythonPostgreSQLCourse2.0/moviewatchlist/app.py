d42ffaa2880824482615f70d1f5cf6b52f7bd988
rominalodolo <lodoloromina@gmail.com>
2022-02-18 10:12:49 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/moviewatchlist/app.py
+++ TheCompletePythonPostgreSQLCourse2.0/moviewatchlist/app.py
@@ -1,3 +1,11 @@
 import database
 
-menu = """Please select one of the """
+menu = """Please select one of the following options:
+1) Add new movie.
+2) View upcoming movies.
+3)
+4)
+5) 
+6) Exit.
+
+Your selection: """
