4f4cfe7d48a8c6a933bcfc371702e82dbf7a9a43
rominalodolo <lodoloromina@gmail.com>
2022-02-18 15:04:37 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/moviewatchlist/app.py
+++ TheCompletePythonPostgreSQLCourse2.0/moviewatchlist/app.py
@@ -14,22 +14,65 @@ welcome = "Welcome to the watchlist app!"
 print(welcome)
 database.create_tables()
 
+def prompt_add_movie():
+	title = input("Movie title: ")
+	release_date = input("Release date (dd-mm-YYYY): ")
+	parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+	timestamp = parsed_date.timestamp()
+
+	database.add_movie(title, timestamp)
+
+def print_movie_list(heading, movies):
+	print(f"{heading} Movies")
+	for _id, title, release_date in movies:
+		movie_date = datetime.datetime.fromtimestamp(release_date)
+		human_date = movie_date.strftime("%b %d %Y")
+		print(f"{_id}: {title} (on {human_date})")
+	print("\n")
+
+def prompt_show_watched_movies():
+	username = input("Username: ")
+	movies = database.get_watched_movies(username)
+	if movies:
+		print_movie_list("Watched", movies)
+	else:
+		print("That user has not been awesome enough to watch a movie yet!")
+
+def prompt_search_movies():
+	search_term = input("Enter the partial movie title: ")
+	movies = database.search_movies(search_term)
+	if movies:
+		print_movie_list("Movies found: ", movies)
+	else:
+		print("Found no movies for that search term!")
+
+def prompt_add_user():
+	username = input("Username: ")
+	database.add_user(username)
+
 def prompt_watch_movie():
     movie_title = input("Enter movie title you've watched: ")
     database.watch_movie(movie_title)
 
 
-while (user_input := input(menu)) != "6":
+while (user_input := input(menu)) != "8":
     if user_input == "1":
         prompt_add_movie()
     elif user_input == "2":
         movies = database.get_movies(True)
         print_movie_list("Upcoming", movies)
     elif user_input == "3":
-        
+        movies = database.get_movies()
+		print_movie_list("All", movies)
     elif user_input == "4":
-        pass
+        prompt_watch_movie()
     elif user_input == "5":
-        pass
+        prompt_show_watched_movies()
+	elif user_input == "6":
+		prompt_add_user()
+	elif user_input == "7":
+		prompt_search_movies()
     else:
         print("Invalid input, please try again!")
+
+
