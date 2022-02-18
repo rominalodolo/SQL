52b0ede8905f75892f0cb3e1849350b2c0e98d10
rominalodolo <lodoloromina@gmail.com>
2022-02-18 10:59:33 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/moviewatchlist/database.py
+++ TheCompletePythonPostgreSQLCourse2.0/moviewatchlist/database.py
@@ -1,5 +1,8 @@
-# title, release_date, watched 
+import
 
+
+from multiprocessing import connection
+import sqlite3
 from tkinter import INSERT
 
 
@@ -12,3 +15,6 @@ CREATE_MOVIES_TABLE = """ CREATE TABLE IS NOT EXISTS movies (
 INSERT_MOVIES = "INSERT INTO movies () VALUES (?, ?, 0);"
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
+SELECT_WATCHED_MOVIES = "SELECT * FROM  movies WHERE watched = 1;"
+
+connection = sqlite3.connect("data.db")
