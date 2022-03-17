4bf7bbbdbed4178ad2ed06670bbb0a68f1fb2f98
rominalodolo <lodoloromina@gmail.com>
2022-03-17 20:53:02 +0200

    update

--- TheCompletePythonPostgreSQLCourse2.0/pollappdates/dates.py
+++ TheCompletePythonPostgreSQLCourse2.0/pollappdates/dates.py
@@ -2,3 +2,12 @@ import pytz
 import json
 
 user_timezone = input("Enter your timezone: ").strip()
+
+try: 
+    pytz.timezone(user_timezone)
+except pytz.exceptions.UnknownTimeZoneError:
+    print("That was not a valid timezone.")
+    raise
+
+with open("user_config.json", "w") as config:
+    json.dump({"timezone": user_timezone}, config)
